aee97f644b9f99c41f908a75e84e4f9cc0f53906
gwako94 <galgallowako254@gmail.com>
2018-09-27 18:36:50 +0300

    [ch-database-setup#160703095]

---
+++ migrations.py
@@ -0,0 +1,64 @@
+"""Implements database"""
+import psycopg2
+import os
+
+
+class Database(object):
+
+    #constructor initialize environment
+    def __init__(self):
+        self.host = os.getenv('HOST')
+        self.dbname = os.getenv('DEV_DB')
+        self.user = os.getenv('USER')
+        self.password = os.getenv('PASSWORD')
+
+        try:
+            self.conn = psycopg2.connect(host=self.host, dbname=self.dbname, user=self.user, password=self.password)
+        except:
+            print("Unable to connect to the database")
+
+        self.cur = self.conn.cursor()
+    
+    def create_tables(self):
+        """ Method to create tables """
+
+        users = '''CREATE TABLE IF NOT EXISTS users(
+                    id SERIAL PRIMARY KEY,
+                    username VARCHAR(50) NOT NULL,
+                    email VARCHAR(70) UNIQUE NOT NULL,
+                    password VARCHAR(250) NOT NULL,
+                    admin bool,
+                    created_at TIMESTAMP
+        );'''
+
+        menu =  '''CREATE TABLE IF NOT EXISTS menu(
+                    menu_id SERIAL PRIMARY KEY,
+                    name VARCHAR(50) NOT NULL,
+                    image VARCHAR(270) NOT NULL,
+                    description VARCHAR(350) NOT NULL,
+                    price numeric NOT NULL,
+                    created_at TIMESTAMP
+                    
+        );'''
+
+        orders = '''CREATE TABLE IF NOT EXISTS orders(
+                    order_id SERIAL PRIMARY KEY,
+                    user_id INTEGER REFERENCES users(id),
+                    cart VARCHAR(200) NOT NULL,
+                    total numeric NOT NULL,
+                    status VARCHAR(20) NOT NULL,
+                    created_at TIMESTAMP,
+                    updated_at TIMESTAMP
+                    
+        );'''
+        queries = [users, menu, orders]
+
+        for query in queries:
+            self.cur.execute(query)
+            self.conn.commit()
+            print("All tables created successfully!")
+        self.cur.close()
+        self.conn.close()
+
+db = Database()
+db.create_tables()

ddbca8cd6c9e1bd7eed4ad33a730743e7a34f823
gwako94 <galgallowako254@gmail.com>
2018-09-29 13:00:10 +0300

    [bg-fix-double-registration ##160794405]

--- app/v2/views/userView.py
+++ app/v2/views/userView.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify, Blueprint, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
+import psycopg2
 import datetime
 from app.v2.models.userModel import User
 from app.v2.utilities import validate_register
@@ -12,13 +13,17 @@ def register_user():
     data = request.get_json()
     if validate_register(data):
         return validate_register(data)
-        
+
     user_details = User(
         data['username'],
         data['email'],
         data['password']
     )
   
-    user_details.register_user()
-    return jsonify({'message': 'User registered successfully!'})
+    try:
+        user_details.register_user()
+        return jsonify({'message': 'User registered successfully!'})
+
+    except psycopg2.IntegrityError:
+        return jsonify({'message': 'User already exists!'})
 
